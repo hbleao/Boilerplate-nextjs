63dabc7af55c0d957d3ef9abc0374356162258e7
hleao <henrique.leao@softvaro.com.br>
2022-08-15 10:14:49 -0300

    feat: lint test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,8 +4,7 @@ import Image from 'next/image';
 import styles from '../styles/Home.module.css';
 
 const Home: NextPage = () => {
-  const blasdfa = '';
-  const blasdfa111 = '';
+  const test = '';
 
   return (
     <div className={styles.container}>
